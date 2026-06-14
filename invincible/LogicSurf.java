07eba477c92e877a2475b6b3ca577975e85a2547
luis Ortega <ortegaluiskb@gmail.com>
2026-06-13 22:00:31 -0600

    Upgrade Lugic surf and errors

--- invincible/LogicSurf.java
+++ invincible/LogicSurf.java
@@ -6,5 +6,27 @@ public class LogicSurf {
 
     public LogicSurf(Hilo hilo) {
         Hilo.count();
+
+        //
+    }
+
+    public void basic_structure() {
+        Hilo.count();
+    }
+
+    public void model(int key) {
+        //
+        if (key >= 0) {
+            Hilo.count();
+            System.out.println("Successful");
+        } else {
+            // pending
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Course of logics";
     }
 }
